f45f24e9238e7103507c094bb1e1b85718f9ad77
yatharthsameer <ytharthsmr@gmail.com>
2022-04-21 17:19:31 +0530

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -136,9 +136,13 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
+              Hellohavfjcbbkjbck ashifhkjabfskjabfcbjkasbjkcbkjabcjkbkj aijsbiabsbfbfibia
+              asufbjabskfbjbas
+              asjfvabsfbsbfiaubfsf
+              afbiausbfbasfnonoansfnoansf
+              sufbiabfbabsjfjajbsjbfdkajb
+              aiufaisbfbasfakjbfjabc
+              g together a custom reblog button taught me a lot
               about HTML &amp; CSS!
             </p>
 
