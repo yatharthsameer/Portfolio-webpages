5d7c0945d903d8c0e672eb3bd6854bf067475c5a
yatharthsameer <ytharthsmr@gmail.com>
2022-04-28 07:51:57 +0530

    cheanged loader logo

--- src/components/loader.js
+++ src/components/loader.js
@@ -29,7 +29,7 @@ const StyledLoader = styled.div`
       margin: 0 auto;
       fill: none;
       user-select: none;
-      #B {
+      #Y {
         opacity: 0;
       }
     }
@@ -53,7 +53,7 @@ const Loader = ({ finishLoading }) => {
         strokeDashoffset: [anime.setDashoffset, 0],
       })
       .add({
-        targets: '#logo #B',
+        targets: '#logo #Y',
         duration: 700,
         easing: 'easeInOutQuart',
         opacity: 1,
